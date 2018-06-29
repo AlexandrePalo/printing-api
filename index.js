4610126e424657f22ffc169eb485adfd1b548abe
Alexandre PALO <a-palo@laposte.net>
2018-06-29 10:58:12 +0200

    adding nodemon and babel envé

--- index.js
+++ index.js
@@ -1,34 +1,42 @@
-const { ApolloServer, gql } = require('apollo-server')
+import { ApolloServer, gql } from 'apollo-server'
+import moment from 'moment'
 
 // This is a (sample) collection of books we'll be able to query
 // the GraphQL server for.  A more complete example might fetch
 // from an existing data source like a REST API or database.
-const books = [
+const serieports = [
   {
-    title: 'Harry Potter and the Chamber of Secrets',
-    author: 'J.K. Rowling'
+    name: '/dev/tty1'
   },
   {
-    title: 'Jurassic Park',
-    author: 'Michael Crichton'
+    name: '/dev/tty2'
+  }
+]
+
+const files = [
+  {
+    name: 'file1',
+    duration: 1000,
+    date: moment().toISOString()
   }
 ]
 
 // Type definitions define the "shape" of your data and specify
 // which ways the data can be fetched from the GraphQL server.
 const typeDefs = gql`
-  # Comments in GraphQL are defined with the hash (#) symbol.
-
-  # This "Book" type can be used in other type declarations.
-  type Book {
-    title: String
-    author: String
+  type SeriePort {
+    name: String
   }
 
-  # The "Query" type is the root of all GraphQL queries.
-  # (A "Mutation" type will be covered later on.)
+  type File {
+    name: String
+    duration: Int
+    date: String
+  }
+
   type Query {
-    books: [Book]
+    serieports: [SeriePort]
+    files: [File]
   }
 `
 
@@ -36,7 +44,8 @@ const typeDefs = gql`
 // schema.  We'll retrieve books from the "books" array above.
 const resolvers = {
   Query: {
-    books: () => books
+    serieports: () => serieports,
+    files: () => files
   }
 }
 
