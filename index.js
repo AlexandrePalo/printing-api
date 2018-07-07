282581efb6fcdf7848ec168b55ad4c71f00c414f
Alexandre PALO <a-palo@laposte.net>
2018-07-07 16:17:57 +0200

    finalize upload

--- index.js
+++ index.js
@@ -3,28 +3,54 @@ import moment from 'moment'
 import fs from 'fs'
 import path from 'path'
 import serialport from 'serialport'
+import forge from 'node-forge'
 import { connectToPrinter } from './src/printer/connexion'
 import { durationAlgorithm } from './src/utils/files'
+import uuidv4 from 'uuid/v4'
+import jwt from 'jsonwebtoken'
 
 let SerialPort = null
+const uploadDir = __dirname + '/public/3D'
 
-const files = [
+const getObjectInArray = (arr, id) => {
+  let object = null
+  arr.forEach(e => {
+    if (e.id === id) {
+      object = e
+    }
+  })
+  return object
+}
+const users = [
   {
-    name: 'file1',
-    duration: 1000,
-    date: moment().toISOString()
+    id: '45745c60-7b1a-11e8-9c9c-2d42b21b1a3e',
+    username: 'apalo',
+    passwordHash:
+      '018c1b59605a109a08fcd7e40e71a877d97f09405cbd2a78c153d30bb4f0540f',
+    name: 'Alexandre PALO'
   }
 ]
+const privateKey =
+  "_a$glTa>^A]2<W/TYw43x!%4y70h?]OY]<AKQW<s~m?blH(d%PKPC'#OFoi%j"
 
-// Type definitions define the "shape" of your data and specify
-// which ways the data can be fetched from the GraphQL server.
 const typeDefs = gql`
+  type User {
+    username: String
+    passwordHash: String
+    name: String
+    id: String
+  }
+
+  type JWT {
+    token: String
+  }
+
   type SeriePort {
     name: String
   }
 
   type File {
-    name: String
+    path: String
     duration: Int
     date: String
   }
@@ -36,12 +62,15 @@ const typeDefs = gql`
 
   type Query {
     serieports: [SeriePort]
-    files(name: String): [File]
   }
 
   type Mutation {
+    login(username: String, password: String): JWT
+    reLogin(token: String): User
     connectToPrinter(port: String, baudRate: Int): PrinterResponse
-    readDir(dir: String): [File]
+    readDir: [File]
+    deleteFile(path: String): File
+    addFile(file: Upload): File
   }
 `
 
@@ -56,12 +85,6 @@ const resolvers = {
           name: r.comName
         }))
       })
-    },
-    files: (obj, args, context, info) => {
-      if (args.name) {
-        return files.filter(f => f.name === args.name)
-      }
-      return files
     }
   },
   Mutation: {
@@ -83,22 +106,188 @@ const resolvers = {
     },
     readDir: (obj, args, context, info) => {
       return new Promise((resolve, reject) => {
-        fs.readdir(args.dir, (err, files) => {
+        fs.readdir(uploadDir, (err, files) => {
           err && reject(err)
           resolve(files)
         })
       }).then(files => {
-        return files.filter(f => path.extname(f) === '.gcode').map(f => ({
-          name: path.basename(f, path.extname(f)),
-          duration: durationAlgorithm(args.dir + '/' + f),
-          date: moment(fs.statSync(args.dir + '/' + f).mtime).toISOString()
-        }))
+        // Authorization
+        if (!context.user) {
+          throw new Error('not authorized')
+        }
+
+        return files.filter(f => path.extname(f) === '.gcode').map(f => {
+          return {
+            path: uploadDir + '/' + f,
+            duration: Math.round(durationAlgorithm(uploadDir + '/' + f)),
+            date: moment(fs.statSync(uploadDir + '/' + f).mtime).toISOString()
+          }
+        })
       })
+    },
+    deleteFile: (obj, args, context, info) => {
+      return new Promise((resolve, reject) => {
+        // Authorization
+        if (!context.user) {
+          throw new Error('not authorized')
+        }
+
+        const deletedFile = {
+          name: args.path,
+          duration: durationAlgorithm(args.path),
+          date: moment(fs.statSync(args.path).mtime).toISOString()
+        }
+
+        fs.unlink(args.path, err => {
+          err && reject(err)
+          resolve(deletedFile)
+        })
+      }).then(file => {
+        return file
+      })
+    },
+    addFile: async (obj, args, context, info) => {
+      // Get file input
+      const input = await args.file
+
+      // Save into server
+      const id = uuidv4()
+      const uploadFilename = `${id}-${input.filename}`
+      const path = `${uploadDir}/${uploadFilename}`
+
+      return new Promise((resolve, reject) =>
+        input.stream
+          .on('error', error => {
+            if (input.stream.truncated)
+              // Delete the truncated file
+              fs.unlinkSync(path)
+            reject(error)
+          })
+          .pipe(fs.createWriteStream(path))
+          .on('error', error => reject(error))
+          .on('finish', () => resolve())
+      )
+        .then(() => {
+          return {
+            path: uploadDir + '/' + uploadFilename,
+            duration: Math.round(
+              durationAlgorithm(uploadDir + '/' + uploadFilename)
+            ),
+            date: moment(
+              fs.statSync(uploadDir + '/' + uploadFilename).mtime
+            ).toISOString()
+          }
+        })
+        .catch(err => {
+          return {
+            path: null,
+            duration: null,
+            date: null
+          }
+        })
+    },
+    login: (obj, args, context, info) => {
+      return new Promise((resolve, reject) => {
+        let user = null
+        users.forEach(u => {
+          if (u.username === args.username) {
+            user = u
+          }
+        })
+        !user && reject('Bad credentials')
+
+        let hash = forge.md.sha256.create()
+        hash.update(args.password)
+        if (hash.digest().toHex() === user.passwordHash) {
+          const token = jwt.sign(user, privateKey)
+          resolve(token)
+        } else {
+          reject('Bad credentials')
+        }
+      })
+        .then(token => {
+          return { token }
+        })
+        .catch(err => {
+          return { message: err }
+        })
+    },
+    reLogin: (obj, args, context, info) => {
+      return new Promise((resolve, reject) => {
+        // Decode jwt and check user
+        let userToken = jwt.decode(args.token)
+        if (!userToken.id || !users.map(u => u.id).includes(userToken.id)) {
+          reject('User not found')
+        }
+        // Get current user in backend
+        const user = getObjectInArray(users, userToken.id)
+        // Verify token
+        if (!jwt.verify(args.token, privateKey)) {
+          reject('Bad token')
+        }
+        // Everything ok, send current user
+        resolve(user)
+      })
+        .then(user => {
+          return user
+        })
+        .catch(err => {
+          return { message: err }
+        })
     }
   }
 }
 
-const server = new ApolloServer({ typeDefs, resolvers })
+const processUpload = async upload => {
+  console.log(upload)
+  const { stream, filename, mimetype, encoding } = await upload
+  console.log(stream, filename, mimetype, encoding)
+
+  const { id, path } = await storeFS({ stream, filename })
+  return storeFS({ id, filename, mimetype, encoding, path })
+}
+
+const storeFS = ({ stream, filename }) => {
+  const id = uuidv4()
+  const path = `${uploadDir}/${id}-${filename}`
+  return new Promise((resolve, reject) =>
+    stream
+      .on('error', error => {
+        if (stream.truncated)
+          // Delete the truncated file
+          fs.unlinkSync(path)
+        reject(error)
+      })
+      .pipe(fs.createWriteStream(path))
+      .on('error', error => reject(error))
+      .on('finish', () => resolve({ id, path }))
+  )
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => {
+    // Authorization requested
+    if (req.headers.authorization) {
+      // Get token
+      const token = req.headers.authorization.split(' ')[1]
+      // Get user and check if exist
+      const userToken = jwt.decode(token)
+      if (!userToken.id || !users.map(u => u.id).includes(userToken.id)) {
+        throw new AuthorizationError('user not found')
+      }
+      // Verify token
+      if (!jwt.verify(token, privateKey)) {
+        throw new AuthorizationError('bad token')
+      }
+      // Return user
+      return { user: getObjectInArray(users, userToken.id) }
+    }
+    return { user: null }
+  }
+})
+server.appl
 
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`)
--- src/utils/files.js
+++ src/utils/files.js
@@ -1,7 +1,107 @@
 import fs from 'fs'
+import lineByLine from 'n-readlines'
+import math from 'mathjs'
 
 const durationAlgorithm = f => {
-  return fs.statSync(f).size
+  /* Estimate the print duration from a gcode file
+  c -> current
+  n -> next
+  */
+  let x_c = 0
+  let y_c = 0
+  let z_c = 0
+  let v_c = 0
+  let d_c = 0
+
+  // Sync line reader
+  const liner = new lineByLine(f)
+  let command
+
+  while ((command = liner.next())) {
+    // Cast buffer string
+    command = command.toString('ascii')
+
+    // Only G0 and G1
+    if (isGMoveCommand(command)) {
+      const params = paramsFromGCommand(command)
+      let x_n = params.X ? params.X.value : x_c
+      let y_n = params.Y ? params.Y.value : y_c
+      let z_n = params.Z ? params.Z.value : z_c
+      let v_n = params.F ? params.F.value : v_c
+
+      d_c =
+        d_c +
+        moveDuration(
+          moveDistance({ x: x_n, y: y_n, z: z_n }, { x: x_c, y: y_c, z: z_c }),
+          v_n / 60
+        )
+
+      params.X && (x_c = params.X.value)
+      params.Y && (y_c = params.Y.value)
+      params.Z && (z_c = params.Z.value)
+      params.F && (v_c = params.F.value)
+    }
+  }
+
+  return d_c
+}
+
+const isGMoveCommand = command => {
+  const re = new RegExp('G([0, 1]{1})', 'gi')
+  if (command.match(re)) {
+    return true
+  }
+  return false
+}
+
+const paramsFromGCommand = command => {
+  /* Extract G, F, X, Y, Z, E info from a G0/1 command
+  Usually G1 are for extrusion, but some slicers doesn't respect the convention.
+  Thus, G1 and G0 are treated in the same way. */
+  return {
+    G: commandParamValue(command, 'G'),
+    F: commandParamValue(command, 'F'),
+    X: commandParamValue(command, 'X'),
+    Y: commandParamValue(command, 'Y'),
+    Z: commandParamValue(command, 'Z'),
+    E: commandParamValue(command, 'E')
+  }
+}
+
+const commandParamValue = (fullCommand, param) => {
+  /* Return an object with the param command and the value */
+
+  let re = null
+  // Integer
+  if (['G'].indexOf(param) !== -1) {
+    re = new RegExp('(' + param + '[0-9]+)', 'gi')
+  }
+  // Decimal
+  if (['F', 'X', 'Y', 'Z', 'E'].indexOf(param) !== -1) {
+    re = new RegExp('(' + param + '[0-9.]+)', 'gi')
+  }
+
+  const raw = fullCommand.match(re)
+  if (raw) {
+    if (raw.length === 1) {
+      return {
+        command: raw[0],
+        value: Number(raw[0].slice(1, raw[0].length))
+      }
+    }
+    return {}
+  }
+}
+
+const moveDistance = (i, j) => {
+  return math.sqrt(
+    math.square(i.x - j.x) + math.square(i.y - j.y) + math.square(i.y - j.y)
+  )
+}
+
+const moveDuration = (distance, speed) => {
+  // mm s
+  return distance / speed
 }
 
 export { durationAlgorithm }
